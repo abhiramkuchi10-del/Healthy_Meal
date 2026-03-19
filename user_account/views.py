29438ec3b97cdaeaaf148807c59bb186e90bce11
Abhiram KKuchi <abhiramk@mustangmath.com>
2026-03-18 22:47:04 -0700

    first commit

---
+++ user_account/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+# Create your views here.
+def register(request):
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
