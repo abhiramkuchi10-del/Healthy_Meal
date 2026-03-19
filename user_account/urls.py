14bd5515f4183429d8f968737ce9f4e89ac656f0
Abhiram KKuchi <abhiramkuchi@gmail.com>
2026-03-19 11:09:00 -0700

    added logout

--- user_account/urls.py
+++ user_account/urls.py
@@ -20,5 +20,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.register, name='signup'),
-    path('login/', views.login_view, name='login')
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout, name='logout')
 ]
--- user_account/views.py
+++ user_account/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 # Create your views here.
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
 def register(request):
     if request.method == "POST":
         first_name = request.POST['first_name']
